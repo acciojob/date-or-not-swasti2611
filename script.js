cc59c2c26779580a17fdc1ddfecf3d79c55d64c9
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-02-17 13:43:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var isDate = function (input) {
   //   write your code here
-};
-
+	let newinput=parse.Date();
+	if(typeof newinput===Date){
+		return true;
+	}else{
+		return false;
+	}
 // Do not change the code below.
-const input = prompt("Enter Date.");
+const input= prompt("Enter Date.");
 alert(isDate(input));
+	
